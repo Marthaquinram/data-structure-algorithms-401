f5d5910cc5be7b5cc63990709029b28db6842198
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-11-08 21:52:45 -0800

    initial commit for CC 30 hashtables

--- javascript/code-challenges/hash-tables/README30.md
+++ javascript/code-challenges/hash-tables/README30.md
@@ -1,11 +1,34 @@
-# Hash Table
+# Challenge 30
 
-## Challenge 30
+## Hash Table-
+
+Implement a Hashtable Class with the following methods:
+
+- set
+  - Arguments: key, value
+  - Returns: nothing
+    This method should hash the key, and set the key and value pair in the table, handling collisions as needed.
+  - Should a given key already exist, replace its value from the value argument given to this method.
+- get
+  - Arguments: key
+  - Returns: Value associated with that key in the table
+- has
+  - Arguments: key
+  - Returns: Boolean, indicating if the key exists in the table already.
+- keys
+  - Returns: Collection of keys
+- hash
+  - Arguments: key
+  - Returns: Index in the collection for that key
 
 ## Whiteboard
 
 - working on this
   ![CC30]()
+
+## Approach & Efficiency
+
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
 
 ## Starting up
 
---
+++ javascript/code-challenges/hash-tables/hashtable.js
@@ -0,0 +1,116 @@
+class HashMap {
+  constructor(size) {
+    this.size = size; // tracks size of our map array
+    this.map = new Array(size).fill(); // create an array with a specified size
+    // this.map = []; // other way of creating an array (don't use this)
+    this.numItems = 0;
+  }
+
+  // add a new { key : value }object to hashmap
+  set(key, value) {
+    let idx = this.hash(key);
+    const object = {}
+    object['key'] = key;
+    object['value'] = value;
+    if (this.map[idx]) {
+      this.map[idx].push(object);
+    } else {
+      this.map[idx] = new Array();
+      this.map[idx].push(object);
+      // this.map[idx] = [[object]];
+      // this.map[idx] = [[key, value]];
+    }
+  }
+
+  get(key) {
+    let idx = this.hash(key);
+    if (this.map[idx]) {
+      let results = [];
+      for (const item of this.map[idx]) {
+        results.push(item.key);
+      }
+      return results;
+      // return item;
+      // const element = this.map[idx].find(x => x.key === key)[1];
+    } else {
+      return undefined;
+    }
+  }
+
+  // returns all unique keys in the hashmap and nested hashmaps
+  keys() {
+    let results = [];
+    let set = new Set();
+    // console.log('map:: ', map);
+    if (this.map.length > 0) {
+      for (let i = 0; i < this.map.length; i++) {
+        if (this.map[i] != undefined) {
+          for (const item of this.map[i]) {
+            if (!set.has(item.key))
+              set.add([i], item.key);
+          }
+        }
+      }
+      set.forEach(element => {
+        results.push(element);
+      });
+      return results;
+      // return map;
+    }
+  }
+
+
+  has(key) {
+    let hashIndex = this.hash(key);
+    console.log('has was called')
+    if (this.map[hashIndex] !== undefined) {
+      // console.log(this.map[hashIndex].length, ' keys in that index');
+      // console.log('map[hashIndex] ---> ', this.map[hashIndex])
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // converts our string input into a random integer that we use for indexing
+  hash(key) {
+    let sumOfKeyLetters = key.split('').reduce((total, currentValue) => {
+      let value = currentValue.charCodeAt(0);
+      let num = total + value;
+      return num;
+    }, 0);
+    let hash = sumOfKeyLetters * 599 % this.size;
+    return hash;
+  }
+}
+
+
+// instantiate a new hashMap & set an index size
+let hashMap = new HashMap(7);
+
+// push/set things to our map
+// console.log('size: ', hashMap.length);
+hashMap.set('Joe', 'Regalado');
+hashMap.set('John', 'Smith');
+hashMap.set('Tony', 'Second');
+hashMap.set('Tony', 'Third');
+hashMap.set('Joe', 'Smith');
+console.log(hashMap.keys());
+
+// return all values at given index with input
+// hashMap.get('Johnny');
+// hashMap.get('Tony');
+
+
+// how to set objects properties and assign them values
+function createAnObject(first, last) {
+  const object = {}
+  let arr = [];
+
+  object['key'] = first;
+  object['value'] = last;
+
+  arr.push(object);
+  console.log(arr);
+  return arr;
+}
